c08fd7feda7014edd72404bfd3406d756f71f03a
Oke253 <salahudeenfataioke@gmail.com>
2022-11-21 20:32:32 +0100

    correction1

--- _printf.c
+++ _printf.c
@@ -17,24 +17,22 @@ int _printf(const char *format, ...)
 	int count = 0;
 	int value = 0;
 	va_list args;
+
 	va_start(args, format);
 	int (*f)(va_list);
-	
 	/*Prevent parsing a null pointer*/
 	if (format == NULL)
 		return (-1);
-	
 	/*Print each character of string*/
 	while (format[i])
-	{	
+	{
 		if (format[i] != '%')
 		{
-			value = write(1,&format[i],1);
+			value = write(1, &format[i], 1);
 			count = count + value;
 			i++;
 			continue;
 		}
-
 		if (format[i] == '%')
 		{
 			f = check_specifier(&format[i + 1]);
@@ -45,23 +43,18 @@ int _printf(const char *format, ...)
 				i = i + 2;
 				continue;
 			}
-
 			if (format[i + 1] == '\0')
 			{
 				break;
 			}
-
 			if (format[i + 1] != '\0')
 			{
 				value = write(1, &format[i + 1], 1);
 				count = count + value;
-                        	i = i + 2;
-                        	continue;
+				i = i + 2;
+				continue;
 			}
-
-
 		}
 	}
-
 	return (count);
 }
