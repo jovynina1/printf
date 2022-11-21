787c58e7ec86978ca53e68e99614c167cc79ec6f
jovynina1 <jovynina@gmail.com>
2022-11-21 10:03:44 +0000

    Files

--- _printf.c
+++ _printf.c
@@ -1,4 +1,106 @@
 #include "main.h"
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * copy_to_buffer - Copies the given character over to the buffer
+ * @formatter: Character to copy over
+ * @buffer: Buffer being copied to
+ * @buflenptr: Pointer to the length of the buffer, the number of
+ * characters in the buffer
+ * @bufposptr: Pointer to the position in the buffer
+ * Return: Number of characters copied to buffer
+*/
+int copy_to_buffer(char formatter, char buffer[],
+				     int *buflenptr, int *bufposptr)
+{
+	int chars;
+	chars = 0;
+	buffer[*bufposptr] = formatter;
+	*bufposptr += 1;
+	*buflenptr += 1;
+	if (*buflenptr == 1024)
+	{
+		write_buffer(buffer, buflenptr, bufposptr);
+	}
+	chars++;
+	return (chars);
+}
+
+/**
+ * check_conversion - Checks formatter character to see if
+ * it's a conversion specifier
+ * @formatter: The format character being checked
+ * @conversions: Struct holding conversion specifiers & function pointers to
+ * @appropriate functions for corresponding conversion specifier
+ * @buffer: Buffer needed to copy to when calling function
+ * @buflenptr: Pointer to the length of the buffer
+ * @bufposptr: Pointer to the position within the buffer
+ * @print_this: va_list holding all given arguments to _printf function
+ * Return: Return the number of characters copied to buffer if a
+ * function is called, 0 if no function is called
+*/
+int check_conversion(char formatter, char_funcs_t conversions[], char buffer[],
+				     int *buflenptr, int *bufposptr, va_list print_this)
+{
+	int j, chars;
+	
+	chars = 0;
+	for (j = 0; j < 13; j++)
+	{
+		if (formatter == *conversions[j].c)
+		{
+			chars += conversions[j].f(print_this, buffer, buflenptr,
+					 bufposptr);
+			return (chars);
+		}
+	}
+	return (0);
+}
+
+/**
+ * formatPrinter - finds the formatters function and prints its arguement
+ * @format: The format character being checked
+ * @conversions: Struct holding conversion specifiers & function pointers to
+ * appropriate functions for corresponding conversion specifier
+ * @buffer: Buffer needed to copy to when calling function
+ * @buflenptr: Pointer to the length of the buffer
+ * @bufposptr: Pointer to the position within the buffer
+ * @print_this: va_list holding all given arguments to _printf function
+ * Return: Return the number of characters copied to buffer if a
+ * function is called, 0 if no function is called
+*/
+int formatPrinter(const char *format, va_list print_this, char buffer[],
+				  int *buflenptr, int *bufposptr, char_funcs_t conversions[])
+{
+	int i, chars, print;
+
+	chars = 0;
+	for (i = 0; format[i] != '\0' && format != NULL; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			print = check_conversion(format[i], conversions,
+					 buffer, buflenptr, bufposptr,
+					 print_this);
+			if (print == 0)
+				chars += copy_to_buffer(format[i], buffer,
+						buflenptr, bufposptr);
+			chars += print;
+		}
+		else
+		{
+			chars += copy_to_buffer(format[i], buffer, buflenptr,
+					bufposptr);
+		}
+	}
+	return (chars);
+}
+
+
+
 
 /**
  * _printf - Receives the main string and all the necessary parameters to
@@ -8,9 +110,13 @@
 */
 int _printf(const char *format, ...)
 {
-	int printed_chars;
-	conver_t f_list[] = {
-		{"c", print_char},
+
+	va_list print_this;
+	char buffer[1024];
+	int chars, buflen, bufpos, *buflenptr, *bufposptr;
+	char_funcs_t conversions[] = {{"c", print_c},
+
+		
 		{"s", print_string},
 		{"%", print_percent},
 		{"d", print_integer},
@@ -24,14 +130,17 @@ int _printf(const char *format, ...)
 		{"X", print_heX},
 		{NULL, NULL}
 	};
-	va_list arg_list;
-
-	if (format == NULL)
-		return (-1);
-
-	va_start(arg_list, format);
-	/*Calling parser function*/
-	printed_chars = parser(format, f_list, arg_list);
-	va_end(arg_list);
-	return (printed_chars);
+	initialize_buffer(buffer);
+	chars = bufpos = 0;
+	buflen = 1;
+	buflenptr = &buflen;
+	bufposptr = &bufpos;
+	va_start(print_this, format);
+	if (format == NULL || print_this == NULL)
+		return (chars);
+	chars = formatPrinter(format, print_this, buffer,
+			  buflenptr, bufposptr, conversions);
+	write_buffer(buffer, buflenptr, bufposptr);
+	va_end(print_this);
+	return (chars);
 }
--- main.h
+++ main.h
@@ -5,72 +5,36 @@
 #include <stdarg.h>
 
 /**
- * struct flags - struct containing flags to "turn on"
- * when a flag specifier is passed to _printf()
- * @plus: flag for the '+' character
- * @space: flag for the ' ' character
- * @hash: flag for the '#' character
+ * struct char_funcs - Struct to find function based on char
+ * @c: char to check as key
+ * @f: Pointer to function to call
 */
-typedef struct flags
+typedef struct char_funcs
 {
-		int plus;
-			int space;
-				int hash;
-} flags_t;
+		char *c;
+			int (*f)();
+} char_funcs_t;
 
-/**
- * struct printHandler - struct to choose the right function depending
- * on the format specifier passed to _printf()
- * @c: format specifier
- * @f: pointer to the correct printing function
-*/
-typedef struct printHandler
-{
-		char c;
-			int (*f)(va_list ap, flags_t *f);
-} ph;
-
-/* print_nums */
-int print_int(va_list l, flags_t *f);
-void print_number(int n);
-int print_unsigned(va_list l, flags_t *f);
-int count_digit(int i);
-
-/* print_bases */
-int print_hex(va_list l, flags_t *f);
-int print_hex_big(va_list l, flags_t *f);
-int print_binary(va_list l, flags_t *f);
-int print_octal(va_list l, flags_t *f);
-
-/* converter */
-char *convert(unsigned long int num, int base, int lowercase);
-
-/* _printf */
+void write_buffer(char buffer[], int *buflen, int *bufpos);
+void initialize_buffer(char buffer[]);
 int _printf(const char *format, ...);
-
-/* get_print */
-int (*get_print(char s))(va_list, flags_t *);
-
-/* get_flag */
-int get_flag(char s, flags_t *f);
-
-/* print_alpha */
-int print_string(va_list l, flags_t *f);
-int print_char(va_list l, flags_t *f);
-
-/* write_funcs */
-int _putchar(char c);
-int _puts(char *str);
-
-/* print_custom */
-int print_rot13(va_list l, flags_t *f);
-int print_rev(va_list l, flags_t *f);
-int print_bigS(va_list l, flags_t *f);
-
-/* print_address */
-int print_address(va_list l, flags_t *f);
-
-/* print_percent */
-int print_percent(va_list l, flags_t *f);
+int print_c(va_list args, char buffer[], int *buflen, int *bufpos);
+int print_s(va_list args, char buffer[], int *buflen, int *bufpos);
+int print_number(int n, char buffer[], int *buflen, int *bufpos);
+int print_int(va_list args, char buffer[], int *buflen, int *bufpos);
+char *binConverter(unsigned int n);
+char *octConverter(unsigned int n);
+char *hexConverter(char type, unsigned int n);
+char *size_tHex(char type, size_t n);
+int print_Unum(unsigned int n, char buffer[], int *buflen, int *bufpos);
+int print_u(va_list args, char buffer[], int *buflen, int *bufpos);
+int print_o(va_list args, char buffer[], int *buflen, int *bufpos);
+int print_hex(va_list args, char buffer[], int *buflen, int *bufpos);
+int print_heX(va_list args, char buffer[], int *buflen, int *bufpos);
+int print_b(va_list args, char buffer[], int *buflen, int *bufpos);
+int print_S(va_list args, char buffer[], int *buflen, int *bufpos);
+int print_r(va_list args, char buffer[], int *buflen, int *bufpos);
+int print_R(va_list args, char buffer[], int *buflen, int *bufpos);
+int print_p(va_list args, char buffer[], int *buflen, int *bufpos);
 
 #endif
